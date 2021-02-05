fb1ea15361c68dc54fcd3fe9e94255f000c25475
ZachZhong <zachx7@yeah.net>
2021-02-05 16:19:53 +0800

    update

--- algorithm/src/main/java/org/zach/base/sort/impl/BubbleSortord.java
+++ algorithm/src/main/java/org/zach/base/sort/impl/BubbleSortord.java
@@ -19,6 +19,7 @@ public class BubbleSortord extends Sortord {
             }
         }
     }
+
     static void swap(int[] arr,int i, int j) {
         arr[i] = arr[i] ^ arr[j];
         arr[j] = arr[i] ^ arr[j];
